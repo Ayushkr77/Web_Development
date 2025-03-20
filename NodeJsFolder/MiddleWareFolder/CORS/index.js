8f62788cef9618b948c2dc55253571096f53e543
Ayush Kumar Sinha <ayushkumar5990@gmail.com>
2025-03-20 19:48:05 +0530

    Learnt CORS

---
+++ NodeJsFolder/MiddleWareFolder/CORS/index.js
@@ -0,0 +1,36 @@
+// Assignment
+// 1. Create a backend server in node js, that returns the sum endpoint
+// 2. Write html file that hits the backend server using fetch api
+
+const express = require("express");
+const cors=require("cors");
+
+const app = express();
+
+app.use(express.json());
+
+// If u want to host both html and js file in the same origin, then here CORS problem will not accept here
+// app.get("/",function(req,res) {   
+//     res.sendFile(__dirname+"/index.html");
+// })
+
+
+// app.use(cors()); // enable CORS for all requests
+// Allow requests ONLY from a specific frontend origin
+app.use(cors({  
+    origin: 'http://127.0.0.1:5500' // will only accept from this origin, not all. cannot write http://localhost:5500
+}));
+
+
+app.post("/sum", function(req, res) {
+    const a = parseInt(req.body.a);
+    const b = parseInt(req.body.b);
+    res.json({
+        ans: a + b
+    })
+    console.log(a+b);
+});
+
+app.listen(3000,()=> {
+    console.log("Server running on port 3000");
+});
